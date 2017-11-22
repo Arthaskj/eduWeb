37c2344f035c60ae1bd1284b100e39d04bc63b52
Arthaskj <1285707427@qq.com>
2017-11-22 17:31:57 +0800

    MobileWeb

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(function(){
+
+// 	var photob = new Array("kind1.png","kind2.png","kind3.png","kind4.png")
+// 	photo($(".content1>.icon>ul img"),photob);
+// 	function photo(attr,array){
+// 		var num = array.length;
+// 		for(var i=0;i<num;i++){
+// 			attr.eq(i).attr("src","img/"+ array[i]);
+// 		}
+// }
+
+
+
+
+
+
+
+})
